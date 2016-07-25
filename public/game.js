90776af6c60dae4cc6e8e96ad43883a8ecc09b2f
Matt Butler <mattb-w@hotmail.co.uk>
2016-07-25 11:51:46 +0100

    new branch

--- public/game.js
+++ public/game.js
@@ -24,9 +24,14 @@
     },
 
     draw: function(screen, gameSize) {
+      screen.clearRect(0, 0, gameSize.x, gameSize.y)
       for (var i = 0; i < this.bodies.length; i++) {
         drawRect(screen, this.bodies[i])
       }
+    },
+
+    addBody: function(body) {
+      this.bodies.push(body)
     }
   }
 
@@ -43,6 +48,42 @@
         this.center.x -= 2
       } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
         this.center.x += 2
+      }
+
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var bullet = new Bullet({ x: this.center.x, y: this.center.y - this.size.x / 2 }, { x: 0, y: -6})
+        this.game.addBody(bullet)
+      }
+
+    }
+  }
+
+  var Bullet = function(center, velocity) {
+    this.size = { x: 3, y: 3}
+    this.center = center
+    this.velocity = velocity
+  }
+
+  Bullet.prototype = {
+    update: function () {
+      this.center.x += this.velocity.x
+      this.center.y += this.velocity.y
+
+    }
+  }
+
+  var Invader = function(game, center) {
+    this.game = game
+    this.size = { x: 15, y: 15}
+    this.center = center
+    this.patrolX = 0
+    this.speedX = 0.3
+  }
+
+  Invader.prototype = {
+    update: function () {
+      if (this.patrolX < 0 || this.patrolX > 40) {
+        this.center.x -= 2
       }
     }
   }
@@ -62,7 +103,7 @@
     }
 
     window.onkeyup = function(e) {
-      keyState[e.keyCode] = true
+      keyState[e.keyCode] = false
     }
 
     this.isDown = function(keyCode) {
